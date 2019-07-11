f9658509af5b705efe3da9749f87caf8b4d75139
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-07-11 14:31:43 +0900

    Add Error page

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="error-page">
+    <h1>Page is not found!</h1>
+    <p class="subheading">お探しのページはみつかりませんでした。</p>
+    <p></p>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
